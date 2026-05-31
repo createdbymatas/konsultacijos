1ca3a5753250487405e4a65f1cb33d5b3dced4be
created by matas <78920383+createdbymatas@users.noreply.github.com>
2026-05-31 16:05:09 +0300

    Add files via upload

--- testavimas/script.js
+++ testavimas/script.js
@@ -37,3 +37,56 @@ function test3(){
         location.replace("test3/english-practise-bandymas-anglu-k-testas-nr-3.html")
     }, 1000);
 }
+
+
+
+function login(){
+    document.getElementById('loginBtn').style.pointerEvents = 'none';
+    document.getElementById('username').style.pointerEvents = 'none';
+    document.getElementById('password').style.pointerEvents = 'none';
+    document.getElementById('status').innerHTML = 'Tikrinami prisijungimo duomenys...';
+    document.getElementById('status').style.opacity = '1';
+    document.getElementById('status').style.color = '#F5F5DC';
+
+    setTimeout(() => {
+        if(document.getElementById("username").value == "RfykyCvo" && document.getElementById("password").value == "g3P$7iF%"){
+            document.getElementById('status').innerHTML = 'Prisijungiama...';
+            setTimeout(() => {
+                location.replace("selection.html")
+            }, 2000);
+        }
+        else if(document.getElementById("username").value == ""){
+            document.getElementById('loginBtn').style.pointerEvents = 'all';
+            document.getElementById('username').style.pointerEvents = 'all';
+            document.getElementById('password').style.pointerEvents = 'all';
+            document.getElementById('status').style.transition = 'none';
+            document.getElementById('status').style.color = 'red';
+            document.getElementById('status').innerHTML = 'Neįvestas prisijungimo vardas';
+            setTimeout(() => {
+                document.getElementById('status').style.transition = '0.2s';
+            }, 100);
+        }
+        else if(document.getElementById("password").value == ""){
+            document.getElementById('loginBtn').style.pointerEvents = 'all';
+            document.getElementById('username').style.pointerEvents = 'all';
+            document.getElementById('password').style.pointerEvents = 'all';
+            document.getElementById('status').style.transition = 'none';
+            document.getElementById('status').style.color = 'red';
+            document.getElementById('status').innerHTML = 'Neįvestas slaptažodis';
+            setTimeout(() => {
+                document.getElementById('status').style.transition = '0.2s';
+            }, 100);
+        }
+        else{
+            document.getElementById('loginBtn').style.pointerEvents = 'all';
+            document.getElementById('username').style.pointerEvents = 'all';
+            document.getElementById('password').style.pointerEvents = 'all';
+            document.getElementById('status').style.transition = 'none';
+            document.getElementById('status').style.color = 'red';
+            document.getElementById('status').innerHTML = 'Neteisingi prisijungimo duomenys';
+            setTimeout(() => {
+                document.getElementById('status').style.transition = '0.2s';
+            }, 100);
+        }
+    }, 3000);
+}
